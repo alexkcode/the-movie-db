fd753bf464e064ad19652dc4fc4f96ca509d7c00
omni <0mniscendanT>
2018-11-12 02:18:45 -0600

    updated dependencies. added first method.

---
+++ demo/src/main/java/com/moviedb/explorer/jobs/ImportMovies.java
@@ -0,0 +1,29 @@
+package com.moviedb.explorer.jobs;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class ImportMovies {
+
+    private String server = "http://";
+    private RestTemplate rest;
+    private HttpHeaders headers;
+
+    public ImportMovies() {
+        this.rest = new RestTemplate();
+        this.headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "*/*");
+    }
+
+    public String get(String uri) {
+        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
+        ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.GET, requestEntity, String.class);
+        return responseEntity.getBody();
+    }
+
+}
